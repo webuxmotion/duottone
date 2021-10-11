945cbaad693fd82b645d0080032d745b358603ff
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2021-10-11 16:15:50 +0300

    Update content

--- app/pages/Lessons/content-items/nuvole-bianche/index.php
+++ app/pages/Lessons/content-items/nuvole-bianche/index.php
@@ -3,6 +3,8 @@
 <p>Ссылка на MIDI - <a href="https://bitmidi.com/ludovico-einaudi-nuvole-bianche-mid">https://bitmidi.com/ludovico-einaudi-nuvole-bianche-mid</a></p>
 
 <p>Фрагмент песни</p>
+<img src="<?=$folder?>/step-47-53.svg" style="width: 100%;" />
 <img src="<?=$folder?>/step-35-40.svg" style="width: 100%;" />
 <img src="<?=$folder?>/step-29-34.svg" style="width: 100%;" />
+
 
